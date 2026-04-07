82b6cb275fee32196fdc0f3b0908aabd80207f9e
Nishadpachghare <pachgharenishad05@gmail.com>
2026-04-07 22:37:39 +0530

    Changes

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ const corsOptions = {
 };
 
 
-app.cors(corsOptions);
+app.use(cors(corsOptions));
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
