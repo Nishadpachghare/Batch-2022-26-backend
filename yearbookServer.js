232bc06c694018d5388741ca771f679977041128
Nishadpachghare <pachgharenishad05@gmail.com>
2026-04-07 12:53:46 +0530

    Update CORS headers for production

--- yearbookServer.js
+++ yearbookServer.js
@@ -14,7 +14,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const PORT = Number(process.env.PORT) || 5000;
 const MONGODB_URI =
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/yearbook";
+  process.env.MONGODB_URI ;
 const FRONTEND_ORIGINS = (
   process.env.FRONTEND_URL || "http://localhost:5173"
 )
